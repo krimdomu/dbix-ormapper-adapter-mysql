ad1bb1ce507cd63812fb82865b1997d2cce2ac09
Jan <krimdomu@googlemail.com>
2011-06-01 21:35:55 +0200

    added DateTime type

---
+++ lib/DM4P/SQL/Dialects/MySQL/Table/Column/Type/DateTime.pm
@@ -0,0 +1,33 @@
+package DM4P::SQL::Dialects::MySQL::Table::Column::Type::DateTime;
+
+use strict;
+use warnings;
+
+use DM4P::SQL::Table::Column::Type::Base;
+
+use base qw(DM4P::SQL::Table::Column::Type::Base);
+
+# ------------------------------------------------------------------------------
+# Group: Constructor
+# ------------------------------------------------------------------------------
+# Function: new
+#
+#   Creates the DM4P::SQL::Dialects::MySQL::Table::Column::Type::DateTime Object.
+#
+# Returns:
+#
+#   DM4P::SQL::Dialects::MySQL::Table::Column::Type::DateTime
+sub new {
+   my $that = shift;
+   my $proto = ref($that) || $that;
+   my $self = $that->SUPER::new(@_);
+   
+   bless($self, $proto);
+   
+   $self->{'__sql_type'} = 'datetime';
+   
+   return $self;
+}
+
+
+1;
