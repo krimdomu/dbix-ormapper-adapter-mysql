74daa5357fb6197511cbe51d9df46c97f4e21242
Jan <krimdomu@googlemail.com>
2011-06-01 21:54:47 +0200

    return a DateTime Object

--- lib/DM4P/SQL/Dialects/MySQL/Table/Column/Type/DateTime.pm
+++ lib/DM4P/SQL/Dialects/MySQL/Table/Column/Type/DateTime.pm
@@ -6,6 +6,7 @@ use warnings;
 use DM4P::SQL::Table::Column::Type::Base;
 
 use base qw(DM4P::SQL::Table::Column::Type::Base);
+use DateTime;
 
 # ------------------------------------------------------------------------------
 # Group: Constructor
@@ -29,5 +30,24 @@ sub new {
    return $self;
 }
 
+
+REDEFINES: {
+   no warnings 'redefine';
+   sub FETCH {
+      my ($self) = @_;
+      my ($year, $mon, $day, $hour, $min, $sec) = split(/[-: ]/, $self->{'__data'});
+      
+      my $dt = DateTime->new(
+         year => $year,
+         month => $mon,
+         day => $day,
+         hour => $hour,
+         minute => $min,
+         second => $sec,
+      );
+
+      return $dt;
+   }
+}
 
 1;
